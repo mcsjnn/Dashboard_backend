28494838c065c59a72c88cb4f6fa786d2cdcce6c
mcsjnn <sujenn.ancco@tecsup.edu.pe>
2025-05-11 22:51:40 -0500

    Configuración de JWT_SECRET con dotenv y variables de entorno

--- app/config/auth.config.js
+++ app/config/auth.config.js
@@ -1,4 +1,8 @@
-// app/config/auth.config.js
+import dotenv from "dotenv";
+
+// Carga las variables de entorno desde el archivo .env (en desarrollo)
+dotenv.config();
+
 export default {
-    secret: "your-secret-key",
-  };
+  secret: process.env.JWT_SECRET || "default-secret-key", // Usa la variable de entorno o una clave por defecto
+};
